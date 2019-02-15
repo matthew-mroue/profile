f299daa700577f804da91e61108b45ab7ed96abd
Matthew Mroue <mamroue@yahoo.com>
2019-02-15 08:59:14 -0500

    second commit

--- app/comptwo/comptwo.component.js
+++ app/comptwo/comptwo.component.js
@@ -3,6 +3,8 @@ const compTwo = {
   template: `
   <div class="profileWrapper">
     <section class="profileContents">
+    <img src="app/assets/IMG_2681.png" alt="" srcset="">
+
       <h1>Name: <span> {{ $ctrl.userProfile.name }} </span></h1>
       <p>Contact: <span> {{ $ctrl.userProfile.contact }} </span></p>
       <p>Bio: <span> {{ $ctrl.userProfile.bio }} </span></p>
